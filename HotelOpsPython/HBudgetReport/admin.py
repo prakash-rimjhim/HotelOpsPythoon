22b55850c64a672b7fd6d1a01c9217c7b3109a61
Himanshu Pal <68378989+himanshu-pal-2002@users.noreply.github.com>
2023-05-24 16:43:57 +0530

    admin changes

--- HotelOpsPython/HBudgetReport/admin.py
+++ HotelOpsPython/HBudgetReport/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Outlet,Expenses,PayrollExpenses
+from .models import Outlet,Expenses,PayrollExpenses,PayrollExpensesEntry
 
 admin.site.register(Outlet)
 admin.site.register(Expenses)
 admin.site.register(PayrollExpenses)
+admin.site.register(PayrollExpensesEntry)
